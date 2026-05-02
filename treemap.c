fe61a8fd253106977bbca7a46b194a64cfef52ad
Your Name <you@example.com>
2026-05-02 17:42:54 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -56,7 +56,7 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 // Si no se encuentra la clave retorna NULL. Recuerde hacer que el current apunte al nodo encontrado.
 
 Pair * searchTreeMap(TreeMap * tree, void* key) {
-    if(tree == NULL) return NULL;
+    if(tree == NULL || tree->root == NULL) return NULL;
     TreeNode* aux = tree->root;
     if(tree->root->pair->key == key) return tree->root->pair;
     while(aux != NULL)
