a0364d8e14f174aa450bfd7e82c914cacef10a99
Your Name <you@example.com>
2026-05-02 15:43:06 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -43,9 +43,12 @@ TreeNode * createTreeNode(void* key, void * value) {
 // Reserve memoria, inicialice el resto de variables y retorne el mapa.
     
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
-
-    //map->lower_than = lower_than;
-    return NULL;
+    TreeMap* mapa = (TreeMap*)malloc(sizeof(TreeMap));
+    if(mapa == NULL) return NULL;
+    mapa->root = NULL;
+    mapa->current = NULL;
+    mapa->lower_than = lower_than;
+    return mapa;
 }
 
 // 2. Implemente la función Pair* searchTreeMap(TreeMap* tree, void* key), 
